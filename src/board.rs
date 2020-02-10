8866fb3aa7304c87e52058e33517a7cbc3139d71
Elisa <elisagougerot@hotmail.fr>
2020-02-10 17:25:29 +0100

    add board and game

---
+++ src/board.rs
@@ -0,0 +1,265 @@
+extern crate opengl_graphics;
+extern crate piston_window;
+extern crate rand;
+
+use opengl_graphics::GlGraphics;
+use piston_window::*;
+
+#[derive(Copy, Clone)]
+pub enum Color {
+    Red,
+    Green,
+    Blue,
+    Magenta,
+    Cyan,
+    Yellow,
+    Orange,
+}
+
+pub struct Metrics {
+    pub block_pixels: usize,
+    pub board_x: usize,
+    pub board_y: usize,
+}
+
+impl Metrics {
+     pub fn resolution(&self) -> [u32; 2] {
+        [
+            (self.board_x * self.block_pixels) as u32,
+            (self.board_y * self.block_pixels) as u32,
+        ]
+    }
+}
+
+type Cell = Option<Color>;
+
+#[derive(Clone)]
+pub struct Board {
+    cells: Vec<Vec<Cell>>,
+}
+
+pub type Piece = Board;
+
+pub enum DrawEffect {
+    None,
+    //Flash(&'a Vec<usize>),
+    Darker,
+}
+
+impl Board {
+    pub fn empty(dim_x: usize, dim_y: usize) -> Self {
+        let line: Vec<_> = (0..dim_x).map(|_| None).collect();
+        let cells: Vec<_> = (0..dim_y).map(|_| line.clone()).collect();
+        Board { cells }
+    }
+
+    pub fn valid(&self, offset: (isize, isize)) -> bool {
+        if offset.0 >= 0 && offset.0 < self.dim_x() as isize {
+            if offset.1 >= 0 && offset.1 < self.dim_y() as isize {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    pub fn dim_x(&self) -> usize {
+        self.cells[0].len()
+    }
+    pub fn dim_y(&self) -> usize {
+        self.cells.len()
+    }
+
+    pub fn piece(spec: &[[u8; 4]; 4], color: Color) -> Self {
+        let mut board = Board::empty(spec[0].len(), spec.len());
+
+        for x in 0..spec[0].len() {
+            for y in 0..spec.len() {
+                board.cells[y][x] = if spec[y][x] != 0 { Some(color) } else { None }
+            }
+        }
+
+        board
+    }
+
+    pub fn as_merged(&self, offset: (isize, isize), board: &Board) -> Option<Board> {
+        let mut copy = self.clone();
+
+        for x in 0..board.dim_x() {
+            for y in 0..board.dim_y() {
+                let cell = board.cells[y][x];
+                if cell.is_some() {
+                    let x = x as isize + offset.0;
+                    let y = y as isize + offset.1;
+                    if !self.valid((x, y)) {
+                        return None;
+                    }
+                    if self.cells[y as usize][x as usize].is_none() {
+                        copy.cells[y as usize][x as usize] = cell.clone();
+                    } else {
+                        // Collision
+                        return None;
+                    }
+                }
+            }
+        }
+
+        Some(copy)
+    }
+
+    pub fn draw(
+        &self,
+        c: &Context,
+        gl: &mut GlGraphics,
+        effect: DrawEffect,
+        metrics: &Metrics,
+    ) {
+        let mut draw = |color, rect: [f64; 4]| {
+            Rectangle::new(color).draw(rect, &DrawState::default(), c.transform, gl);
+        };
+
+        for x in 0..self.dim_x() {
+            for y in 0..self.dim_y() {
+                let block_pixels = metrics.block_pixels as f64;
+                let border_size = block_pixels / 20.0;
+                let outer = [
+                    block_pixels * (x as f64),
+                    block_pixels * (y as f64),
+                    block_pixels,
+                    block_pixels,
+                ];
+                let inner = [
+                    outer[0] + border_size,
+                    outer[1] + border_size,
+                    outer[2] - border_size * 2.0,
+                    outer[3] - border_size * 2.0,
+                ];
+
+                draw([0.2, 0.2, 0.2, 1.0], outer);
+                draw([0.1, 0.1, 0.1, 1.0], inner);
+
+                self.cells[y][x].map(|color| {
+                    let code = match color {
+                        Color::Red => [1.0, 0.0, 0.0, 1.0],
+                        Color::Green => [0.0, 1.0, 0.0, 1.0],
+                        Color::Blue => [0.5, 0.5, 1.0, 1.0],
+                        Color::Magenta => [1.0, 0.0, 1.0, 1.0],
+                        Color::Cyan => [0.0, 1.0, 1.0, 1.0],
+                        Color::Yellow => [1.0, 1.0, 0.0, 1.0],
+                        Color::Orange => [1.0, 0.5, 0.0, 1.0],
+                    };
+
+                    draw(code, outer);
+
+                    let code = [code[0] * 0.8, code[1] * 0.8, code[2] * 0.8, code[3]];
+
+                    draw(code, inner);
+                });
+
+                match effect {
+                    DrawEffect::None => {}
+                    //DrawEffect::Flash(lines) => {
+                        //if lines.contains(&(y as usize)) {
+                            //draw([1.0, 1.0, 1.0, 0.5], outer);
+                        //}
+                    //}
+                    DrawEffect::Darker => {
+                        draw([0.0, 0.0, 0.0, 0.9], outer);
+                    }
+                }
+            }
+        }
+    }
+
+    pub fn without_line(&self, idx: usize) -> Self {
+        let mut board = self.clone();
+
+        board.cells.remove(idx);
+        board
+    }
+
+    pub fn prepend_empty_line(&self) -> Self {
+        let line: Vec<_> = (0..self.dim_x()).map(|_| None).collect();
+        let mut board = self.clone();
+
+        board.cells.insert(0, line);
+        board
+    }
+
+    pub fn with_eliminate_lines(&self, lines: &Vec<usize>) -> Self {
+        let mut board = self.clone();
+
+        for idx in lines {
+            board = board.without_line(*idx);
+        }
+
+        for _ in 0..lines.len() {
+            board = board.prepend_empty_line();
+        }
+
+        board
+    }
+
+    pub fn with_trimmed_lines(&self) -> Self {
+        let mut board = self.clone();
+
+        while board.cells[0].iter().all(Cell::is_none) {
+            board = board.without_line(0);
+        }
+
+        while board.cells[board.dim_y() - 1].iter().all(Cell::is_none) {
+            board = board.without_line(board.dim_y() - 1);
+        }
+
+        board
+    }
+
+    pub fn get_full_lines_indicts(&self) -> Vec<usize> {
+        self.cells
+            .iter()
+            .enumerate()
+            .rev()
+            .filter(|(_, line)| line.iter().all(|cell| !cell.is_none()))
+            .map(|(idx, _)| idx)
+            .collect()
+    }
+
+    pub fn transposed(&self) -> Self {
+        let mut board = Self::empty(self.dim_y(), self.dim_x());
+
+        for x in 0..self.dim_x() {
+            for y in 0..self.dim_y() {
+                board.cells[x][y] = self.cells[y][x];
+            }
+        }
+
+        board
+    }
+
+    pub fn with_mirrored_y(&self) -> Self {
+        let mut board = Self::empty(self.dim_x(), self.dim_y());
+
+        for x in 0..self.dim_x() {
+            for y in 0..self.dim_y() {
+                board.cells[y][x] = self.cells[y][self.dim_x() - x - 1];
+            }
+        }
+
+        board
+    }
+
+    pub fn with_rotated_counter(&self) -> Self {
+        self.transposed().with_mirrored_y()
+    }
+
+    pub fn with_rotated(&self) -> Self {
+        self.with_mirrored_y().transposed()
+    }
+
+    pub fn with_trim_sides(&self) -> Self {
+        self.with_trimmed_lines()
+            .transposed()
+            .with_trimmed_lines()
+            .transposed()
+    }
+}
---
+++ src/game.rs
@@ -0,0 +1,277 @@
+extern crate opengl_graphics;
+extern crate piston_window;
+extern crate rand;
+
+use opengl_graphics::GlGraphics;
+use piston_window::*;
+use std::time::{Duration, Instant};
+
+use board;
+
+pub struct Falling {
+    offset: (isize, isize),
+    piece: board::Piece,
+    time_since_fall: Instant,
+}
+
+enum State {
+    Falling(Falling),
+    Flashing(isize, Instant, Vec<usize>),
+    GameOver,
+}
+
+pub struct Game {
+    board: board::Board,
+    metrics: board::Metrics,
+    possible_pieces: Vec<board::Board>,
+    state: State,
+}
+
+impl Game {
+    pub fn new(metrics: board::Metrics) -> Self {
+        let __ = 0;
+        let xx = 01;
+        let possible_pieces = vec![
+            board::Board::piece(
+                &[
+                    [__, __, __, __],
+                    [__, xx, xx, xx],
+                    [__, xx, __, __],
+                    [__, __, __, __],
+                ],
+                board::Color::Orange,
+            ),
+            board::Board::piece(
+                &[
+                    [__, __, __, __],
+                    [__, xx, xx, xx],
+                    [__, __, __, xx],
+                    [__, __, __, __],
+                ],
+                board::Color::Yellow,
+            ),
+            board::Board::piece(
+                &[
+                    [__, __, __, __],
+                    [xx, xx, xx, xx],
+                    [__, __, __, __],
+                    [__, __, __, __],
+                ],
+                board::Color::Blue,
+            ),
+            board::Board::piece(
+                &[
+                    [xx, xx, xx, __],
+                    [__, xx, __, __],
+                    [__, __, __, __],
+                    [__, __, __, __],
+                ],
+                board::Color::Green,
+            ),
+            board::Board::piece(
+                &[
+                    [__, __, __, __],
+                    [__, xx, xx, __],
+                    [xx, xx, __, __],
+                    [__, __, __, __],
+                ],
+                board::Color::Cyan,
+            ),
+            board::Board::piece(
+                &[
+                    [__, __, __, __],
+                    [xx, xx, __, __],
+                    [__, xx, xx, __],
+                    [__, __, __, __],
+                ],
+                board::Color::Magenta,
+            ),
+            board::Board::piece(
+                &[
+                    [__, __, __, __],
+                    [__, xx, xx, __],
+                    [__, xx, xx, __],
+                    [__, __, __, __],
+                ],
+                board::Color::Red,
+            ),
+        ]
+        .into_iter()
+        .map(|x| x.with_trim_sides())
+        .collect();
+
+        Game {
+            board: board::Board::empty(metrics.board_x, metrics.board_y),
+            state: State::Falling(Self::new_falling(&possible_pieces)),
+            possible_pieces,
+            metrics,
+        }
+    }
+
+    pub fn new_falling(possible_pieces: &Vec<board::Board>) -> Falling {
+        let idx = rand::random::<usize>() % possible_pieces.len();
+
+        Falling {
+            offset: (0, 0),
+            piece: possible_pieces[idx].clone(),
+            time_since_fall: Instant::now(),
+        }
+    }
+
+    pub fn move_piece(&mut self, change: (isize, isize)) {
+        let opt_new_state = match &mut self.state {
+            State::GameOver | State::Flashing(_, _, _) => None,
+            State::Falling(falling) => {
+                let new_offset = {
+                    let (x, y) = falling.offset;
+                    ((x as isize + change.0), (y as isize + change.1))
+                };
+                let is_down = change == (0, 1);
+
+                if self.board.as_merged(new_offset, &falling.piece).is_none() {
+                    //There were collisions
+                    if is_down {
+                        match self.board.as_merged(falling.offset, &falling.piece) {
+                            None => Some(State::GameOver),
+                            Some(merged_board) => {
+                                let completed = merged_board.get_full_lines_indicts();
+                                self.board = merged_board;
+
+                                *falling = Self::new_falling(&self.possible_pieces);
+                                if completed.len() > 0 {
+                                    Some(State::Flashing(0, Instant::now(), completed))
+                                } else {
+                                    None
+                                }
+                            }
+                        }
+                    } else {
+                        None
+                    }
+                } else {
+                    // Keep falling
+                    falling.offset = new_offset;
+                    if is_down {
+                        falling.time_since_fall = Instant::now();
+                    }
+                    None
+                }
+            }
+        };
+
+        if let Some(new_state) = opt_new_state {
+            self.state = new_state;
+        }
+    }
+
+    pub fn rotate(&mut self, counter: bool) {
+        match &mut self.state {
+            State::GameOver | State::Flashing(_, _, _) => {}
+            State::Falling(falling) => {
+                let rotated_piece = if counter {
+                    falling.piece.with_rotated()
+                } else {
+                    falling.piece.with_rotated_counter()
+                };
+
+                self.board
+                    .as_merged(falling.offset, &rotated_piece)
+                    .map(|_| {
+                        falling.piece = rotated_piece;
+                    });
+            }
+        }
+    }
+
+    pub fn progress(&mut self) {
+        enum Disposition {
+            ShouldFall,
+            NewPiece(board::Board),
+        }
+
+        let disp = match &mut self.state {
+            State::GameOver => return,
+            State::Flashing(stage, last_stage_switch, lines) => {
+                if last_stage_switch.elapsed() <= Duration::from_millis(50) {
+                    return;
+                }
+                if *stage < 18 {
+                    *stage += 1;
+                    *last_stage_switch = Instant::now();
+                    return;
+                } else {
+                    Disposition::NewPiece(self.board.with_eliminate_lines(lines))
+                }
+            }
+            State::Falling(falling) => {
+                if falling.time_since_fall.elapsed() <= Duration::from_millis(700) {
+                    return;
+                }
+                Disposition::ShouldFall
+            }
+        };
+
+        match disp {
+            Disposition::ShouldFall => self.move_piece((0, 1)),
+            Disposition::NewPiece(new_board) => {
+                self.board = new_board;
+                self.state = State::Falling(Self::new_falling(&self.possible_pieces));
+            }
+        }
+    }
+
+    pub fn render(&self, gl: &mut GlGraphics, args: &RenderArgs) {
+        let res = self.metrics.resolution();
+        let c = &Context::new_abs(res[0] as f64, res[1] as f64);
+
+        gl.draw(args.viewport(), |_, gl| match &self.state {
+            State::Flashing(stage, _, _) => {
+                let effect = {
+                    if *stage % 2 == 0 {
+                        board::DrawEffect::None
+                    } else {
+                        board::DrawEffect::None
+                    }
+                };
+                self.board.draw(c, gl, effect, &self.metrics);
+            }
+            State::Falling(falling) => {
+                if let Some(merged) = self.board.as_merged(falling.offset, &falling.piece) {
+                    merged.draw(c, gl, board::DrawEffect::None, &self.metrics);
+                }
+            }
+            State::GameOver => {
+                self.board.draw(c, gl, board::DrawEffect::Darker, &self.metrics);
+            }
+        });
+    }
+
+    pub fn on_press(&mut self, args: &Button) {
+        match args {
+            Button::Keyboard(key) => {
+                self.on_key(key);
+            }
+            _ => {}
+        }
+    }
+
+    fn on_key(&mut self, key: &Key) {
+        let movement = match key {
+            Key::Right => Some((1, 0)),
+            Key::Left => Some((-1, 0)),
+            Key::Down => Some((0, 1)),
+            _ => None,
+        };
+
+        if let Some(movement) = movement {
+            self.move_piece(movement);
+            return;
+        }
+
+        match key {
+            Key::Up => self.rotate(false),
+            Key::NumPad5 => self.rotate(true),
+            _ => return,
+        }
+    }
+}
